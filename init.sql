e58c1f5eced0f1e1ee9c522880a1cccc58e04536
unknown <xeugenzx@yandex.ru>
2023-10-23 10:56:22 +0300

    update3

--- init.sql
+++ init.sql
@@ -37,3 +37,11 @@ CREATE TABLE IF NOT EXISTS loans (
   FOREIGN KEY (reader_id) REFERENCES readers(reader_id),
   FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
+
+CREATE TABLE IF NOT EXISTS author_books (
+  author_id INT,
+  book_id INT,
+  PRIMARY KEY (author_id, book_id),
+  FOREIGN KEY (author_id) REFERENCES authors(author_id),
+  FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
